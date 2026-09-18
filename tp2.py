f5d5132d80fe7cab1a346d10ac9276b5298e50e5
NlsHub <nilscoudry@gmail.com>
2026-09-18 15:17:38 +0200

    Exo6

--- tp2.py
+++ tp2.py
@@ -124,3 +124,22 @@ def affichagePolynome(polynome):
 
 print(affichagePolynome([5,[6,[8,[3,[]]]]]))
 
+#Exercice 6 :
+
+import requests 
+
+def get_university_data(country = "France"):
+    url = f"http://universities.hipolabs.com/search?country={country}"
+
+    rawdata = requests.get(url)
+
+    if not rawdata:
+        raise Exception
+
+    data = rawdata.json()
+    return data
+
+
+if __name__ == "__main__":
+    uni_data = get_university_data("France")
+    print(uni_data)
